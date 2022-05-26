53b76177cf2cef2f9c9f16c4745f3734a18cd29b
studio-nakaji <nnnn00223399@gmail.com>
2022-05-26 10:36:43 +0900

    仮にスタッフ管理アプリを切り離し

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ from access_sheets_dir import access_bg_sheet_app
 
 
 def main():
-    contents = {"作業時間の取得":get_work_time,"作業料金csvの取得":access_bg_sheet_app}
+    contents = {"作業時間の取得":get_work_time}
+    # ,"作業料金csvの取得":access_bg_sheet_app}
     st.sidebar.title("アプリを選択")
     selection = st.sidebar.radio("Go to",list(contents.keys()))
     page = contents[selection]
