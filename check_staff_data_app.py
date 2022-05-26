4e56f39ddf41c5a90e0d6bfcb0f96302cebba35e
studio-nakaji <nnnn00223399@gmail.com>
2022-05-26 12:22:14 +0900

    ファイル名変更

---
+++ check_staff_data_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+# import get_work_time
+# from access_sheets_dir import access_bg_sheet_app
+
+
+def main():
+    # contents = {"作業時間の取得":get_work_time}
+    # ,"作業料金csvの取得":access_bg_sheet_app}
+    # st.sidebar.title("アプリを選択")
+    # selection = st.sidebar.radio("Go to",list(contents.keys()))
+    # page = contents[selection]
+    # page.main()
+    st.write("テス")
+
+
+
+if __name__ == '__main__':
+    main()
