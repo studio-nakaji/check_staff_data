34a70fdac1b15407727aaa29b1a60840ca4974f3
studio-nakaji <nnnn00223399@gmail.com>
2022-05-26 10:44:34 +0900

    仮表示

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,13 @@ from access_sheets_dir import access_bg_sheet_app
 
 
 def main():
-    contents = {"作業時間の取得":get_work_time}
+    # contents = {"作業時間の取得":get_work_time}
     # ,"作業料金csvの取得":access_bg_sheet_app}
-    st.sidebar.title("アプリを選択")
-    selection = st.sidebar.radio("Go to",list(contents.keys()))
-    page = contents[selection]
-    page.main()
+    # st.sidebar.title("アプリを選択")
+    # selection = st.sidebar.radio("Go to",list(contents.keys()))
+    # page = contents[selection]
+    # page.main()
+    st.write("テスト")
 
 
 
