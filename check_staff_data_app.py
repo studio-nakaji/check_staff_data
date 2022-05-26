a36e87fffec7be8137c7d97dcdc48d2f86bff773
studio-nakaji <nnnn00223399@gmail.com>
2022-05-26 14:52:59 +0900

    1stcommit

--- check_staff_data_app.py
+++ check_staff_data_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-# import get_work_time
+from streamlit.cli import main
+# import get_work_time_app
 # from access_sheets_dir import access_bg_sheet_app
 
 
 def main():
-    # contents = {"作業時間の取得":get_work_time}
+    # contents = {"作業時間の取得":get_work_time_app}
     # ,"作業料金csvの取得":access_bg_sheet_app}
     # st.sidebar.title("アプリを選択")
     # selection = st.sidebar.radio("Go to",list(contents.keys()))
