fd35f0e53b0c0ab11d28e6780552389fb6138bd4
studio-nakaji <nnnn00223399@gmail.com>
2022-06-07 19:17:23 +0900

    secrets.tomlの追加とcheck_app.pyの更新

--- check_staff_data_app.py
+++ check_staff_data_app.py
@@ -1,17 +1,18 @@
 import streamlit as st
-# from streamlit.cli import main
+from streamlit.cli import main
 # import get_work_time_app
-# from access_sheets_dir import access_bg_sheet_app
+# import access_bg_sheet_app
 
 
 def main():
     # contents = {"作業時間の取得":get_work_time_app}
-    # ,"作業料金csvの取得":access_bg_sheet_app}
+    #             # ,"作業料金csvの取得":access_bg_sheet_app}
     # st.sidebar.title("アプリを選択")
     # selection = st.sidebar.radio("Go to",list(contents.keys()))
     # page = contents[selection]
     # page.main()
-    st.write("テス")
+    sec = st.secrets["GoogleCalenderKey"]
+    st.write(sec)
 
 
 
