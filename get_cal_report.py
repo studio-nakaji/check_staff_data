2782664d5a81d20daf4382e903f2a74ee5e6ac3f
studio-nakaji <nnnn00223399@gmail.com>
2022-06-08 17:13:52 +0900

    pyファイルを全てup

--- get_cal_report.py
+++ get_cal_report.py
@@ -11,9 +11,6 @@ from googleapiclient.errors import HttpError
 import calendar as cal
 import streamlit as st
 
-import toml
-
-
 
 def get_gcal_main(set_year,set_month):
     #カレンダーIDのリスト
@@ -22,10 +19,7 @@ def get_gcal_main(set_year,set_month):
                 {"mai_ID":"c_pc8j2tb1hue45v4adq4g7t0tnc@group.calendar.google.com"},
                 {"haikai_ID":"c_90ih641bbpif2h1vp3p0l6vi5k@group.calendar.google.com"}
             ]
-    
     SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-    
-    
     CAL_ID = id_dict[1]["mai_ID"]
     
     first_hms = "T00:00:00z"                    #0時0分0秒
@@ -41,26 +35,27 @@ def get_gcal_main(set_year,set_month):
     print(f"{set_year}年{set_month}月の合計作業時間")
     
     #ローカル設定
-    # CLIENT_SECRET_FILE = '/Volumes/GoogleDrive/マイドライブ/MyScript/My_Python/Nstudio/client_secret_cal.json'
-    # creds = None
+    CLIENT_SECRET_FILE = '/Volumes/GoogleDrive/マイドライブ/MyScript/My_Python/Nstudio/check_staff_data/client_secret_cal.json'
+    creds = None
     # ファイルtoken.jsonは、ユーザーのアクセストークンと更新トークンを保存し、
     # 認証フローが初めて完了すると自動的に作成されます。
-    # if os.path.exists("token.json"):
-        # creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # 使用可能な（有効な）クレデンシャルがない場合は、ユーザーにログインさせます。
-    # if not creds or not creds.valid:
-    #     print("有効なクレデンシャルがない為、ログインして下さい")
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             CLIENT_SECRET_FILE, SCOPES)
-    #         creds = flow.run_local_server(port=0)
-    #     # 次の実行のためにクレデンシャルを保存します
-    #     with open('token.json', 'w') as token:
-    #         token.write(creds.to_json())
+    if not creds or not creds.valid:
+        print("有効なクレデンシャルがない為、ログインして下さい")
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRET_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # 次の実行のためにクレデンシャルを保存します
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
     
     #streamlit cloud用設定
+    # import toml
     # with open("secrets.toml") as f:
     #     secrets = toml.load(f)["GoogleCalenderKey"]
     secrets = st.secrets["GoogleCalenderKey"]
