7745885ee8e97707be0e83368243d202bf0196fd
studio-nakaji <nnnn00223399@gmail.com>
2022-06-19 15:37:52 +0900

    work_dicの中で[]の内容をリスト化して仮表示

--- get_work_time_app.py
+++ get_work_time_app.py
@@ -140,10 +140,12 @@ def main():
             st.write("おっと、指定の期間中にはデータが見当たらないようです・・・。")
             
     #2022年1月以外を選択し、かつ取得した労働項目が0以上あれば
+    selection_list = [i.split("]"[0].split("[")[1]) for i in list(work_dic) if "]" in i]
     if not(select_y == 2022 and select_m == 1) and len(list(work_dic)) > 0:
         ex2 = st.expander("選択した項目の合計時間を表示")
         multi_work_type = ex2.multiselect(label="予定を選択",options=list(work_dic))
         if len(multi_work_type)>0:
+            ex2.write(selection_list)
             total = 0
             for i in multi_work_type:
                 total += work_dic[i]
