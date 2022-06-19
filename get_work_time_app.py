5c7a4bcd3c4f69d2167c79c1b7b89f6eec8d6cb9
studio-nakaji <nnnn00223399@gmail.com>
2022-06-19 15:46:21 +0900

    work_dicの中で]を含むもののみリスト化

--- get_work_time_app.py
+++ get_work_time_app.py
@@ -140,7 +140,7 @@ def main():
             st.write("おっと、指定の期間中にはデータが見当たらないようです・・・。")
             
     #2022年1月以外を選択し、かつ取得した労働項目が0以上あれば
-    selection_list = list(work_dic)
+    selection_list = [i for i in list(work_dic) if "]" in i]
     if not(select_y == 2022 and select_m == 1) and len(list(work_dic)) > 0:
         ex2 = st.expander("選択した項目の合計時間を表示")
         ex2.write(selection_list)
